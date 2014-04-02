2bb43abcfb767b633795c3c591b4dfbd36934565
logevery <logevery@gmail.com>
2014-04-03 00:49:36 +0800

    shell.sh: change indent

--- .shell.sh
+++ .shell.sh
@@ -132,36 +132,36 @@ alias uuc='sudo umount /mnt/uc'
 
 usb()
 {
-        if [[ ! -d "/mnt/ub" ]]
-        then
-                sudo mkdir /mnt/ub
-        fi
+	if [[ ! -d "/mnt/ub" ]]
+	then
+		sudo mkdir /mnt/ub
+	fi
 
-        if [[ -d $1 ]]
-        then
-                usb_dev=$1
-        else
-                usb_dev="sdb1"
-        fi
+	if [[ -d $1 ]]
+	then
+		usb_dev=$1
+	else
+		usb_dev="sdb1"
+	fi
 
-        sudo mount -o iocharset=utf8 /dev/$usb_dev /mnt/ub/
+	sudo mount -o iocharset=utf8 /dev/$usb_dev /mnt/ub/
 }
 
 usc()
 {
-        if [[ ! -d "/mnt/uc" ]]
-        then
-                sudo mkdir /mnt/uc
-        fi
+	if [[ ! -d "/mnt/uc" ]]
+	then
+		sudo mkdir /mnt/uc
+	fi
 
-        if [[ -d $1 ]]
-        then
-                usb_dev=$1
-        else
-                usb_dev="sdc1"
-        fi
+	if [[ -d $1 ]]
+	then
+		usb_dev=$1
+	else
+		usb_dev="sdc1"
+	fi
 
-        sudo mount -o iocharset=utf8 /dev/$usb_dev /mnt/uc/
+	sudo mount -o iocharset=utf8 /dev/$usb_dev /mnt/uc/
 }
 
 alias v='sudo vim'
@@ -305,9 +305,9 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 
 laa()
 {
-        sudo /opt/lampp/lampp start
-        sudo mkdir /run/mysqld
-        sudo ln -sf /opt/lampp/var/mysql/mysql.sock /run/mysqld/mysqld.sock
+	sudo /opt/lampp/lampp start
+	sudo mkdir /run/mysqld
+	sudo ln -sf /opt/lampp/var/mysql/mysql.sock /run/mysqld/mysqld.sock
 }
 
 alias loo='sudo /opt/lampp/lampp stop'
@@ -345,27 +345,27 @@ TERM=xterm-256color
 
 if [[ -n $DISPLAY ]]
 then
-        BROWSER=chromium
-        have_xmodmap=$(xmodmap | grep "Caps_Lock (0x9)" | wc -l)
-        if [[ $have_xmodmap == 0 ]]
-        then
-                xmodmap ~/.Xmodmap
-        fi
+	BROWSER=chromium
+	have_xmodmap=$(xmodmap | grep "Caps_Lock (0x9)" | wc -l)
+	if [[ $have_xmodmap == 0 ]]
+	then
+		xmodmap ~/.Xmodmap
+	fi
 
-        tmux_count=$(tmux ls | wc -l)
-        if [[ "$tmux_count" == "0" ]]; then
-                tmux -2
-        else
-                if [[ -z "$TMUX" ]]; then
-                        if [[ "$tmux_count" == "1" ]]; then
-                                session_id=1
-                        else
-                                session_id=`echo $tmux_count`
-                        fi
-                        tmux new-session -d -s $session_id
-                        tmux attach-session -t $session_id
-                fi
-        fi
+	tmux_count=$(tmux ls | wc -l)
+	if [[ "$tmux_count" == "0" ]]; then
+		tmux -2
+	else
+		if [[ -z "$TMUX" ]]; then
+			if [[ "$tmux_count" == "1" ]]; then
+				session_id=1
+			else
+				session_id=`echo $tmux_count`
+			fi
+			tmux new-session -d -s $session_id
+			tmux attach-session -t $session_id
+		fi
+	fi
 elif [[ $(ps -e | grep awesome) < 1 ]]
 then
 	startx
@@ -374,49 +374,49 @@ fi
 # Extract Files
 ex()
 {
-        if [ -f $1 ] ; then
-                case $1 in
-                        *.jar)                jar -xvf $1                ;;
-                        *.tar.bz2)   tar xvjf $1    ;;
-                        *.tar.gz)    tar xvzf $1    ;;
-                        *.tar.xz)    tar xvJf $1    ;;
-                        *.bz2)       bunzip2 $1     ;;
-                        *.rar)       unrar x $1     ;;
-                        *.gz)        gunzip $1      ;;
-                        *.tar)       tar xvf $1     ;;
-                        *.tbz2)      tar xvjf $1    ;;
-                        *.tgz)       tar xvzf $1    ;;
-                        # *.zip)       unzip $1       ;;
-                        *.zip)                 7z x $1        ;;
-                        *.Z)         uncompress $1  ;;
-                        *.7z)        7z x $1        ;;
-                        *.xz)        unxz $1        ;;
-                        *.exe)       cabextract $1  ;;
-                        *)           echo "'$1': unrecognized file compression" ;;
-                esac
-        else
-                echo "\`$1' is not a valid file"
-        fi
+	if [ -f $1 ] ; then
+		case $1 in
+			*.jar)                jar -xvf $1                ;;
+			*.tar.bz2)   tar xvjf $1    ;;
+			*.tar.gz)    tar xvzf $1    ;;
+			*.tar.xz)    tar xvJf $1    ;;
+			*.bz2)       bunzip2 $1     ;;
+			*.rar)       unrar x $1     ;;
+			*.gz)        gunzip $1      ;;
+			*.tar)       tar xvf $1     ;;
+			*.tbz2)      tar xvjf $1    ;;
+			*.tgz)       tar xvzf $1    ;;
+			# *.zip)       unzip $1       ;;
+			*.zip)                 7z x $1        ;;
+			*.Z)         uncompress $1  ;;
+			*.7z)        7z x $1        ;;
+			*.xz)        unxz $1        ;;
+			*.exe)       cabextract $1  ;;
+			*)           echo "'$1': unrecognized file compression" ;;
+		esac
+	else
+		echo "\`$1' is not a valid file"
+	fi
 }
 
 pbz()
 {
-        if [[ -f $1 ]]
-        then
-                tar -jcvf $1.tar.bz2 $1
-        fi
+	if [[ -f $1 ]]
+	then
+		tar -jcvf $1.tar.bz2 $1
+	fi
 }
 
 rb()
 {
-        # ~/yunio/script/user_off
-        sudo reboot
+	# ~/yunio/script/user_off
+	sudo reboot
 }
 
 off()
 {
-        # ~/yunio/script/user_off
-        sudo poweroff
+	# ~/yunio/script/user_off
+	sudo poweroff
 }
 
 
